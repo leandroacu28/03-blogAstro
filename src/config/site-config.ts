17a9076f7a9f866e9a4fb980b43290b0197cf9ff
Leandro <findtech.sl@gmail.com>
2024-09-29 23:40:23 -0300

    blog-astro-Glob/Props terminado

---
+++ src/config/site-config.ts
@@ -0,0 +1,6 @@
+
+
+export const siteConfig ={
+    title:"Leandro Blog",
+    description:"Blog Oficial de Leandro Acuña-web Developer",
+}
---
+++ src/utils/formatter.ts
@@ -0,0 +1,14 @@
+import { number } from "astro:schema";
+
+
+export class Formatter{
+    static formatDate(value:Date):string {
+        const date=new Date(value);
+        
+        return Intl.DateTimeFormat('es-ES',{
+            year:'numeric',
+            month:'long',
+            day: '2-digit'
+        }).format(date);
+    }
+}
