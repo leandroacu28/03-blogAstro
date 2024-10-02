98d7d1141901771c273bd6a338094aad9a3867f8
Leandro <findtech.sl@gmail.com>
2024-10-02 13:41:49 -0300

    Componente Image en astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,15 +3,38 @@ import { defineCollection, z } from "astro:content";
 
 
 
+// const blogCollection = defineCollection({
+//     //hay 2 tipos de colecciones content(archivos que tienen markdawn, mdx que se puede renderizar)
+//     //data(metadata, osea la informacion en bruto, como por ejemplo un JSON o YAML)
+//     type:'content',
+//     schema: z.object({
+//     title: z.string(),
+//     date: z.date(), 
+//     description: z.string(),
+//     image: z.string(),
+
+//     // Relacion
+//     author: z.string(),
+
+//     // Relacion
+//     tags: z.array(z.string()), 
+//     }),
+// });
+
+// IMAGEN EN COLECCION
 const blogCollection = defineCollection({
     //hay 2 tipos de colecciones content(archivos que tienen markdawn, mdx que se puede renderizar)
     //data(metadata, osea la informacion en bruto, como por ejemplo un JSON o YAML)
+    //Necesitamos decirle a Astro que la imagen no es un string sino que es una imangen dentro de src
     type:'content',
-    schema: z.object({
-    title: z.string(),
-    date: z.date(), 
-    description: z.string(),
-    image: z.string(),
+    schema: ({image})=> 
+    z.object({
+        title: z.string(),
+        date: z.date(), 
+        description: z.string(),
+        image:image().refine((img)=>img.width<1200,{
+            message:'Image should be lower than 1200px',
+        }),
 
     // Relacion
     author: z.string(),
@@ -20,6 +43,7 @@ const blogCollection = defineCollection({
     tags: z.array(z.string()), 
     }),
 });
+
 
 // exportamos la coleccion, le vamos a poder nombre blog que va apuntar al blog colection
 //la informacion la vamos a grabar en un directorio dentro de content que tiene que tener el nombre blog
